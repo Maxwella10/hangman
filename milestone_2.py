796802ef59ce71c156ef2aa4cdee7f298b76c393
Maxwella10 <96734464+Maxwella10@users.noreply.github.com>
2023-10-11 20:55:30 +0100

    updating random module in the file

--- milestone_2.py
+++ milestone_2.py
@@ -1,8 +1,11 @@
-# Step 1: Create a list containing the names of your 5 favorite fruits.
+# Write import random on the line. Note: To import a module, you have to use the import keyword at the top of the file.
+import random
+# Step 2: Create a list containing the names of your 5 favorite fruits.
 favorite_fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
 
-# Step 2: Assign this list to a variable called word_list.
+# Step  3 & 4: Create the random.choice method and pass the word_list variable into the choice method.
 word_list = favorite_fruits
+word = random.choice(word_list)
 
-# Step 3: Print out the newly created list to the standard output (screen).
-print(word_list)
+# Step 5: Print out word to the standard output. Run the code several times and observe the words printed out after each run.
+print(word)
